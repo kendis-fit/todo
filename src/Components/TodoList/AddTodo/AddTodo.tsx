9fa54f4c036312f59e35d69e2d33c921bee25b7a
Kurt Endless <kendis_it@hotmail.com>
2020-05-05 01:34:56 +0300

    update: validation for AddTodo

--- src/Components/TodoList/AddTodo/AddTodo.tsx
+++ src/Components/TodoList/AddTodo/AddTodo.tsx
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import { Add } from "@material-ui/icons";
-import { TextField, IconButton, Grid, InputAdornment } from "@material-ui/core";
+import { TextField, IconButton, InputAdornment } from "@material-ui/core";
 
 import IAddTodo from "./Interfaces/IAddTodo";
 
 const AddTodo = (props: IAddTodo) => {
 
     const [todo, setTodo] = useState("");
+    const [error, setError] = useState("");
 
     const addTodo = () => {
-        props.onAdd(todo);
-        setTodo("");
+        if (todo) {
+            props.onAdd(todo);
+            setTodo("");
+            setError("");
+        } else {
+            setError("Todo must be filled");
+        }
     }
 
     return(
         <TextField
             label="Todo"
             value={todo}
+            error={!!error}
+            helperText={error || " "}
             fullWidth
             onChange={e => setTodo(e.currentTarget.value)}
             InputProps={{
